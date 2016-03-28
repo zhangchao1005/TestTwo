f95e50cd22c3c0e7cdf64823ae952264748eef0a
dllo <1917143414@qq.com>
2016-03-28 15:51:03 +0800

    按钮完成

--- app/src/main/java/com/lanou/dllo/testtwo/MainActivity.java
+++ app/src/main/java/com/lanou/dllo/testtwo/MainActivity.java
@@ -2,13 +2,24 @@ package com.lanou.dllo.testtwo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btn;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "df564a65f46a4fg564", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
